a857c217f6eb4cd9ced59ec74cd44134863f844e
Mabintosh <mabs@aMacHasNoName.local>
2020-08-07 22:48:59 +0100

    msg

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -35,10 +35,10 @@ exports.handler = function(event, context, callback) {
     .then( data => {
             console.log(`Submitted to Buttondown: ${bodyToPost.email}`)
             console.log(`Response: ${data.json()}`)
-            return {
+            return callback(null, {
                 statusCode: 201,
                 body: String(data)
-            }
+            })
     })
     .catch ( error => {
         // Error 😨
@@ -46,22 +46,22 @@ exports.handler = function(event, context, callback) {
             console.log("data: " + error.response.data);
             console.log(error);
             console.log("status: " + error.response.status);
-            return {
+            return callback(null, {
                 statusCode: error.response.status,
                 body: String(error.response.data)
-            }
+            })
         } else if (error.request) {
             console.log(error.request);
-            return {
+            return callback(null, {
                 statusCode: 502,
                 body: String(error.request)
-            }
+            })
         } else {
             console.log('Error', error.message);
-            return {
+            return callback(null, {
                 statusCode: 500,
                 body: String(error.message)
-            }
+            })
         }
     })
 }
